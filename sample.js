83e03f08e3a319bd945b86ec4177879dc2cffd10
Rohit Dalal <dalalrohit102@gmail.com>
2019-01-08 13:58:36 +0530

    AOS,sidenav.close()

--- sample.js
+++ sample.js
@@ -1,20 +1,45 @@
 $(document).ready(function () {
-    $('.sidenav').sidenav();
 
-    $('.desktop-dropdown').on('click', function () {
-        $('.sidenav').sidenav(close());
-    });
+    //AOS
+    AOS.init();
+
+    // MATERIALIZE INITIALIZATION
+    $('.sidenav').sidenav({
+        draggable: true,
+        inDuration: 250,
+        outDuration: 250
+    }).on('click tap', 'li a', () => {
+        $('.sidenav').sidenav('close');
+    });;
     $(".dropdown-trigger").dropdown();
 
-    //
+
+    // FOR aboutLink and teamLink click to scroll part
+    $('#aboutLink').on('click', function () {
+        $('html, body').animate({
+            scrollTop: $("#about").offset().top - 64
+        }, 1500);
+    })
+    $('#teamLink').on('click', function () {
+        $('html, body').animate({
+            scrollTop: $("#team").offset().top - 64
+        }, 1500);
+    })
+
+
+    //ADDING BLACK BG_COLOR TO NAVBAR AFTER 300px SCROLL FROM TOP
     $(window).scroll(function () {
-        if ($(window).scrollTop() > 400) {
+        if ($(window).scrollTop() > 300) {
             $('.nav-wrapper').addClass('black');
+            // $('.brand-logo > #mylogo').addClass('animated zoomIn');
         }
         else {
             $('.nav-wrapper').removeClass('black');
+            // $('.brand-logo > #mylogo').removeClass('animated zoomIn');
         }
     });
+
+    // FORCE PAGE TO LOAD FROM TOP (0,0) ON EVERY LOAD
     window.onbeforeunload = function () {
         window.scrollTo(0, 0);
     }
@@ -23,7 +48,7 @@ $(document).ready(function () {
         $("html").scrollTop(0);
         // body...
         $('html,body').animate({
-            scrollTop: $('main').offset().top - 100
+            scrollTop: $('main').offset().top
         }, 1000);
     })
 });
